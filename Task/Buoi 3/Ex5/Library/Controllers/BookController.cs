05f545648ac015c8e45927b9b7c2f61bbc1ab24c
Thành An <thanhan1552006@gmail.com>
2026-05-29 11:19:25 +0700

    Add EX 5

---
+++ Task/Buoi 3/Ex5/Library/Controllers/BookController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+
+namespace Library.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Tạo danh sách sách mẫu
+            var books = new List<Book>
+            {
+                new Book { Id = 1, Name = "Clean Code", Price = 2000},
+                new Book { Id = 2, Name = "ASP.NET MVC", Price = 1500},
+                new Book { Id = 3, Name = "Design Pattern", Price = 2500}
+            };
+             //Truyền danh sách sang View
+             return View(books);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            //Tạo lại ds mẫu
+            var books = new List<Book>
+            {
+                new Book { Id = 1, Name = "Clean Code", Price = 2000},
+                new Book { Id = 2, Name = "ASP.NET MVC", Price = 1500},
+                new Book { Id = 3, Name = "Design Pattern", Price = 2500}
+            };
+
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            //Truyền sách sang View
+            return View(book);
+        }
+        public IActionResult Create()
+        {
+            return View(); //Tìm đến Create.cshtml để chạy
+        }
+        //POST: Book/Create
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            //Thông báo thành công
+            TempData["SuccessMessage"] = "Thêm sách \"" + book.Name + "\" thành công!";
+            return RedirectToAction("Create"); //Quay lại trang tạo sách sau khi thêm thành công
+        }
+    }
+}
