d337ff943aa5ce773ffda0acd5ba0c2ce4758a8b
Thành An <thanhan1552006@gmail.com>
2026-06-09 11:33:05 +0700

    Add ORM EX

---
+++ Task/Day 6 ORM/Library/Models/Book.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+namespace Library.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Tên sách không được để trống")]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue, ErrorMessage = "Giá sách phải lớn hơn 0")] //Quy tắc validation
+        [Required(ErrorMessage = "Giá sách không được để trống")]
+        public int Price { get; set; }
+
+
+    }
+}
